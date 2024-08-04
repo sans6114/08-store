4b4fed9f6f69d854af567719d9a2c22257d51dd6
Santiago Sosa <santisosalp@gmail.com>
2024-08-04 18:12:54 -0300

    problemas con get-car-action

--- src/actions/cookies/get-cart-action.ts
+++ src/actions/cookies/get-cart-action.ts
@@ -10,32 +10,46 @@ import {
 import type { CartItem } from '@/interfaces';
 
 export const getCart = defineAction({
-    accept: 'json',
-    //input: z.any(): acciones tienen acceso a las cookies,
-    handler: async (_, {cookies}) => {
-    
-        const cart = JSON.parse(cookies.get('cart')?.value ?? '[]') as CartItem[]
+  accept: 'json',
+  //input: z.any(): acciones tienen acceso a las cookies,
+  handler: async (_, { cookies }) => {
 
-        if(!cart) {
-            return []
-        }
+    const cart = JSON.parse(cookies.get('cart')?.value ?? '[]') as CartItem[]
 
-//products
-        const productId = cart.map(cartItem => cartItem.productId)
-        const dbProducts = await db.select()
-          .from(Products)
-          .innerJoin(ProductImage, eq(Products.id, ProductImage.productId))
-          .where(
-           inArray(Products.id, productId)
-          );
-        
-
-          console.log(dbProducts)
-
-
-
-      return { ok: true, msg: 'Usuario cerrado' }
+    if (!cart) {
+      return []
     }
-  })
-  
-  
+
+    //products
+    const productIds = cart.map((item) => item.productId);
+    const dbProducts = await db.select().from(Products).innerJoin(ProductImage, eq(Products.id, ProductImage.productId)).where(inArray(Products.id, productIds));
+
+
+    console.log(dbProducts)
+
+
+
+    return cart.map((item) => {
+      const dbProduct = dbProducts.find((p) => p.Products.id === item.productId);
+      if (!dbProduct) {
+        throw new Error(`Product with id ${item.productId} not found`);
+      }
+
+      const { title, price, slug } = dbProduct.Products;
+      const image = dbProduct.ProductImage.image;
+
+      return {
+        productId: item.productId,
+        title: title,
+        size: item.size,
+        quantity: item.quantity,
+        image: image.startsWith('http')
+          ? image
+          : `${import.meta.env.PUBLIC_URL}/images/products/${image}`,
+        price: price,
+        slug: slug,
+      };
+    });
+  },
+})
+
--- src/actions/index.ts
+++ src/actions/index.ts
@@ -3,18 +3,22 @@ import {
   logout,
   registerUser,
 } from './auth';
+import { getCart } from './cookies/get-cart-action';
 import {
   getProductBySlug,
   getProductsByPage,
 } from './products';
 
 export const server = {
-  // actions
-
-  // Auth
-  loginUser,
-  logout,
-  registerUser,
-  getProductsByPage,
-  getProductBySlug
-};
+    // actions
+  
+    // Auth
+    loginUser,
+    logout,
+    registerUser,
+    //products
+    getProductsByPage,
+    getProductBySlug,
+    //cart 
+    getCart
+  };
